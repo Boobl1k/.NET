2bd1e190d19b1916905d118372b23cc11d8a1666
Mansur <ahmatowmansur@gmail.com>
2021-10-07 21:10:36 +0300

    new console proj for entryPoint

---
+++ HM5/StarterConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FSLibraryResult;
+
+namespace StarterConsole
+{
+    public static class Program
+    {
+        public static Exception NotEnoughArgs = new Exception("not enough arguments");
+        
+        public static void Main(string[] args)
+        {
+            if (args.Length < 3)
+                throw NotEnoughArgs;
+            
+            var num1Res = ParserFs.ParseNumber(args[0]);
+            var num2Res = ParserFs.ParseNumber(args[2]);
+            var operationRes = ParserFs.ParseCalculatorOperation(args[1]);
+
+            if (num1Res.IsError)
+                throw new Exception(num1Res.ErrorValue);
+            if (num2Res.IsError)
+                throw new Exception(num2Res.ErrorValue);
+            if (operationRes.IsError)
+                throw new Exception(operationRes.ErrorValue);
+
+            var num1 = num1Res.ResultValue;
+            var num2 = num2Res.ResultValue;
+            var operation = operationRes.ResultValue;
+
+            var calculationRes = CalculatorFs.Calculate(num1, num2, operation);
+
+            if (calculationRes.IsError)
+                throw new Exception(calculationRes.ErrorValue);
+
+            var result = calculationRes.ResultValue;
+            
+            Console.WriteLine($"result is {result}");
+        }
+    }
+}
