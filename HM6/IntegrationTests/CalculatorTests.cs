82f03a6fcbb48112099bce6ffba547c22eba7126
Mansur <ahmatowmansur@gmail.com>
2021-10-20 11:16:56 +0300

    поменял проект тестов на xunit, с использованием WebApplicationFactory. тесты на работают

--- HM6/IntegrationTests/CalculatorTests.cs
+++ HM6/IntegrationTests/CalculatorTests.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -13,13 +14,15 @@ namespace IntegrationTests
         [Test]
         public async Task Test1()
         {
-            var httpClient = new HttpClient();
+            var hostBuilder = new WebApplicationFactory<Startup>();
+            
+            var httpClient = hostBuilder.CreateClient();
 
             var response = await httpClient.GetAsync("http://localhost:5000/calc?v1=1&v2=2&op=sum");
 
             var t = await response.Content.ReadAsStringAsync();
             
-            Assert.AreEqual('3',  t[0]);
+            Assert.AreEqual(3m,  decimal.Parse(t, CultureInfo.InvariantCulture));
         }
     }
 }
