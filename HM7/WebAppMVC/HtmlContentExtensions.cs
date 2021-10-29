7e1e36ffc153ef51c95aa9d75ad3b94620d203be
Мансур Ахматов <ahmatowmansur@gmail.com>
2021-10-29 14:01:11 +0300

    почистил код

--- HM7/WebAppMVC/HtmlContentExtensions.cs
+++ HM7/WebAppMVC/HtmlContentExtensions.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Text.Encodings.Web;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Html;
 using WebAppMVC.Models;
 
@@ -18,60 +16,36 @@ namespace WebAppMVC
 
         private class FormContent : IHtmlContent
         {
-            private readonly Task<string> _resultContentTask;
+            private readonly string _resultContent;
 
             public FormContent(IModelForEditorForm model) =>
-                _resultContentTask = Task.Run(() => CreateContent(model.GetType().GetProperties()));
+                _resultContent = CreateContent(model.GetType().GetProperties());
 
-            private static string CreateContent(IEnumerable<PropertyInfo> propertyInfos)
-            {
-                var rb = new StringBuilder();
+            private static string CreateContent(IEnumerable<PropertyInfo> propertyInfos) =>
+                propertyInfos
+                    .Select(x =>
+                        "<div class=\"editor-field\">" +
+                        CreateHeaderForProperty(x) +
+                        CreateBodyForProperty(x) +
+                        "</div>")
+                    .Aggregate(string.Concat);
 
-                foreach (var propertyInfo in propertyInfos)
-                {
-                    rb.Append("<div class=\"editor-field\">");
+            private static string CreateHeaderForProperty(PropertyInfo prop) =>
+                $"<div class=\"editor-label\"><label for=\"{prop.Name}\">{prop.Name}</label></div>";
 
-                    rb.Append(CreateHeaderForProperty(propertyInfo));
-                    rb.Append(CreateBodyForProperty(propertyInfo));
-
-                    rb.Append("</div>");
-                }
-
-                return rb.ToString();
-            }
-
-            private static string CreateHeaderForProperty(PropertyInfo prop)
-            {
-                var res = string.Empty;
-
-                res += "<div class=\"editor-label\">";
-                res += $"<label for=\"{prop.Name}\">{prop.Name}</label>";
-                res += "</div>";
-                
-                return res;
-            }
-            private static string CreateBodyForProperty(PropertyInfo prop)
-            {
-                var result = string.Empty;
-                
-                if (prop.PropertyType.IsAssignableTo(typeof(Enum)))
-                {
-                    result += "<select class=\"form-group\">";
-                    result += $"<option selected>{prop.Name}</option>";
-                    result += prop.PropertyType
-                        .GetFields()
-                        .Where(m => m.Name != "value__")
-                        .Select(field => $"<option value=\"{field.Name}\">{field.Name}</option>")
-                        .Aggregate(string.Concat);
-                    result += "</select>";
-                }
-                else if (IsNumber(prop.PropertyType))
-                    result += $"<input class=\"text-box single-line\" type=\"number\" name=\"{prop.Name}\">";
-                else
-                    result += $"<input class=\"text-box single-line\" type=\"text\" name=\"{prop.Name}\">";
-
-                return result;
-            }
+            private static string CreateBodyForProperty(PropertyInfo prop) =>
+                prop.PropertyType.IsAssignableTo(typeof(Enum))
+                    ? "<select class=\"form-group\">"
+                      + $"<option selected>{prop.Name}</option>"
+                      + prop.PropertyType
+                          .GetFields()
+                          .Where(m => m.Name != "value__")
+                          .Select(field => $"<option value=\"{field.Name}\">{field.Name}</option>")
+                          .Aggregate(string.Concat)
+                      + "</select>"
+                    : IsNumber(prop.PropertyType)
+                        ? $"<input class=\"text-box single-line\" type=\"number\" name=\"{prop.Name}\">"
+                        : $"<input class=\"text-box single-line\" type=\"text\" name=\"{prop.Name}\">";
 
             private static readonly Type[] numberTypesArray =
             {
@@ -90,8 +64,8 @@ namespace WebAppMVC
 
             private static bool IsNumber(Type type) => numberTypesArray.Any(type.IsAssignableTo);
 
-            public async void WriteTo(TextWriter writer, HtmlEncoder encoder) =>
-                await writer.WriteLineAsync(await _resultContentTask);
+            void IHtmlContent.WriteTo(TextWriter writer, HtmlEncoder encoder) =>
+                writer.WriteLine(_resultContent);
         }
     }
 }
